99724c145f9292aa6cb7c1d8019614efd7e7c6ff
JoseNarvaja <josegastonnarvaja@gmail.com>
2023-09-23 16:13:58 -0300

    Se agrega modelo para reserva de libros

---
+++ BookReservationAPI/Models/Reservation.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookReservationAPI.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime ReservationDate { get; set; }
+        [Required]
+        public DateTime ReservationEnd { get; set; }
+        public DateTime PickupDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+        [Required]
+        [ForeignKey("BookId")]
+        public Book Book { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        [ForeignKey("UserId")]
+        public LocalUser User { get; set; }
+    }
+}
